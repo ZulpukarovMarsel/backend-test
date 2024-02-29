04ebf1393d0f6e62e2c8a88efcaf8b513177262e
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-29 21:40:13 +0600

    added admin panel

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Post, Comment, Like
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['photo', 'author', 'description', 'created_at']
+    list_filter = ['created_at', 'author']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'author', 'text', 'created_at']
+    list_filter = ['author', 'created_at', 'post']
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['post', 'user']
+    list_filter = ['post', 'user']
--- config/settings/base.py
+++ config/settings/base.py
@@ -1,10 +1,10 @@
+import os
 from pathlib import Path
 from config.settings.env_reader import env, csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 WSGI_APPLICATION = 'config.wsgi.application'
 PRODUCTION = env("PRODUCTION", default=False, cast=bool)
-
 
 THIRD_PARTY_APPS = [
     'rest_framework',
@@ -86,7 +86,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -98,12 +97,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+from config.settings.jazzmin import *
 
 if PRODUCTION:
     from .production import *
