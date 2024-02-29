da76b7bb482516736563699472ffea32e73ab0c8
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-29 19:58:33 +0600

    create models(Post, Comment, Like0

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    photo = models.ImageField(upload_to='post_photo')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+    def __str__(self):
+        return f'Фото{self.photo} - Автор{self.author}'
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Комментария"
+        verbose_name_plural = "Комментарии"
+
+    def __str__(self):
+        return f'Пост{self.post} - Автор{self.author}'
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Пост{self.post} - Автор{self.user}'
+    class Meta:
+        verbose_name = "Лайк"
+        verbose_name_plural = "Лайки"
