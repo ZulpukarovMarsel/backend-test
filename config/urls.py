ccf90434f4aaeb790b449bd6aee13a8a967a4b52
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-03-01 00:40:23 +0600

    update urls

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from config.settings import base
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.posts.urls'))
 ]
+
+if base.DEBUG:
+    urlpatterns += static(base.MEDIA_URL, document_root=base.MEDIA_ROOT)
